dde9c8855e2d7e1ae1ef4d2c6a568b8083543a0d
OksanaTryhub <108359208+OksanaTryhub@users.noreply.github.com>
2024-04-10 13:55:36 +0300

    Updated Home Page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,13 @@
+'use client';
+
 import MeetingTypeList from "@/components/MeetingTypeList";
 
 const Home = () => {
   const now = new Date();
   const time = now.toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit'});
   const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
+
+  console.log(time);
 
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
